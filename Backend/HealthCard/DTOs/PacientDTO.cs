12673f018091e5ddb88c72acdc0da767d1a3297a
Zarzand <Zarzand>
2020-05-10 11:20:39 +0300

    First commit

---
+++ Backend/HealthCard/DTOs/PacientDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProiectMDS.Models
+{
+    public class PacientDTO
+    {
+        public int PacientId { get; set; }
+        public string PacientNume { get; set; }
+        public string PacientPrenume { get; set; }
+        public int Varsta { get; set; }
+        public string CNP { get; set; }
+    }
+}
